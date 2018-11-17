0d64ff7e9d9f7d174f563f3cc73b1c18a54653df
David Sabalete <dsabalete@gmail.com>
2018-11-17 23:16:21 +0100

    Configure Switch route

--- src/app.js
+++ src/app.js
@@ -6,44 +6,44 @@ import './styles/styles.scss';
 
 const ExpenseDashboardPage = () => (
     <div>
-      This is from my dashboard component
+        This is from my dashboard component
     </div>
-  );
-  
-  const AddExpensePage = () => (
-    <div>
-      This is from my add expense component
-    </div>
-  );
-  
-  const EditExpensePage = () => (
-    <div>
-      This is from my edit expense component
-    </div>
-  );
-  
-  const HelpPage = () => (
-    <div>
-      This is from my help component
-    </div>
-  );
+);
 
-  const NotFoundPage = () => (
+const AddExpensePage = () => (
     <div>
-      404!
+        This is from my add expense component
     </div>
-  );
-  
-  const routes = (
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help component
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+);
+
+const routes = (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
-  );
-  
-  ReactDOM.render(routes, document.getElementById('app'));
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
